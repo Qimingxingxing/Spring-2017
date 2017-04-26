1e9c6d53e35a383ce7c5b1c80da556e7c2649202
qimingxingxing <qz718@nyu.edu>
2017-04-26 12:54:50 -0400

    homework 11

--- homework/week11/src/main/java/edu/nyu/cs9053/homework11/network/GameClient.java
+++ homework/week11/src/main/java/edu/nyu/cs9053/homework11/network/GameClient.java
@@ -2,8 +2,11 @@ package edu.nyu.cs9053.homework11.network;
 
 import edu.nyu.cs9053.homework11.game.Difficulty;
 import edu.nyu.cs9053.homework11.game.GameProvider;
+import edu.nyu.cs9053.homework11.game.screen.InputMove;
 
 import java.io.*;
+import java.net.Socket;
+import java.nio.charset.Charset;
 
 /**
  * User: blangel
@@ -15,11 +18,55 @@ import java.io.*;
  */
 public class GameClient implements GameProvider {
 
+    private final Difficulty difficulty;
+    private static final Charset CHARSET = Charset.forName("UTF-8");
+    private final InputStream clientInput;
+    private final OutputStream clientOutput;
+    
     public static GameClient construct(Difficulty difficulty) {
-        // TODO - construct and return the GameClient - do not keep this comment in your code
+        try {
+            Socket socket = new Socket(GameServer.SERVER_HOST, GameServer.SERVER_PORT);
+            return new GameClient(socket.getInputStream(), socket.getOutputStream(), difficulty);
+        } catch (IOException ioe) {
+            System.out.println("construct client failed!");
+            throw new RuntimeException(ioe);
+        }
     }
 
-    public GameClient(Difficulty difficulty, InputStream serverInput, OutputStream serverOutput) {
+    public GameClient(InputStream clientInput, OutputStream clientOutput, Difficulty difficulty) {
+        this.clientInput = clientInput;
+        this.clientOutput = clientOutput;
+        this.difficulty = difficulty;
     }
 
+    @Override
+    public int getRandomNumberOfNextFoes() {
+        int numberOfNextFoes = 0;
+        try {
+            clientOutput.write(getDifficulty().name().getBytes(CHARSET));
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(clientInput));
+            numberOfNextFoes = Integer.parseInt(inputReader.readLine());
+        } catch (IOException ioe) {
+            System.out.println("Get num of next foes failed!");
+        }
+        return numberOfNextFoes;
+
+    }
+    @Override
+    public InputMove getRandomNextMove() {
+        InputMove nextMove = null;
+        try {
+            clientOutput.write("Move".getBytes(CHARSET));
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(clientInput));
+            nextMove = InputMove.valueOf(inputReader.readLine());
+        } catch (IOException ioe) {
+            System.out.println("get next move failed");
+        }
+        return nextMove;
+    }
+
+    @Override
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
 }
--- homework/week11/src/main/java/edu/nyu/cs9053/homework11/network/GameServer.java
+++ homework/week11/src/main/java/edu/nyu/cs9053/homework11/network/GameServer.java
@@ -1,6 +1,15 @@
 package edu.nyu.cs9053.homework11.network;
 
+import edu.nyu.cs9053.homework11.game.Difficulty;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.*;
 
 /**
  * User: blangel
@@ -23,8 +32,15 @@ import java.io.IOException;
 public class GameServer implements NetworkGameProvider, Runnable {
 
     public static final String SERVER_HOST = "localhost";
-
     public static final int SERVER_PORT = 8080;
+    private final ByteBuffer buffer;
+    private final int BUFFERSIZE = 1024;
+    private final Charset CHARSET = Charset.forName("UTF-8");
+    private final Selector selector;
+    private final ServerSocketChannel serverSocketChannel;
+    // map the client socketchannel with the write buffer
+    private final Map<SocketChannel, ByteBuffer> map;
+    private final Random RANDOM;
 
     public static void main(String[] args) throws IOException {
         GameServer server = new GameServer();
@@ -32,7 +48,115 @@ public class GameServer implements NetworkGameProvider, Runnable {
     }
 
     public GameServer() throws IOException {
+    	RANDOM = new Random();
+        map = new HashMap<>();
+        buffer = ByteBuffer.allocate(BUFFERSIZE);
+        selector = Selector.open();
+        serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new java.net.InetSocketAddress(SERVER_HOST, SERVER_PORT));
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+    }
+
+
+    @Override
+    public void run(){
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                int numberOfReadyChannels = selector.select();
+                if (numberOfReadyChannels == 0) {
+                    continue;
+                }
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+                while (keyIterator.hasNext()) {
+                    try {
+                        SelectionKey key = keyIterator.next();
+                        if (key.isAcceptable()) {
+                            // accept the channel which is ready for new connection
+                            SocketChannel clientChannel = serverSocketChannel.accept();
+                            clientChannel.configureBlocking(false);
+                            clientChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+                            map.put(clientChannel, ByteBuffer.allocate(BUFFERSIZE));
+                        }
+                        else if (key.isReadable()) {
+                            // when any channel is ready for read
+                            SocketChannel clientChannel = (SocketChannel) key.channel();
+                            buffer.clear();
+                            try {
+                                clientChannel.read(buffer);
+                                String data = new String(buffer.array(), CHARSET);
+                                ByteBuffer writeBuffer = map.get(clientChannel);
+                                if (!data.substring(0, 4).equals("Move")) {
+                                    // if a call using String "Easy" would be a call to getRandomNextMove
+                                    writeBuffer.put(ByteBuffer.wrap(getRandomNumberOfNextFoes(data).getBytes(CHARSET)));
+                                    writeBuffer.put((byte) '\n');
+
+                                } else {
+                                    // if a call using String "move" would be a call to getRandomNextMove
+                                    writeBuffer.put(ByteBuffer.wrap(getRandomNextMove().getBytes(CHARSET)));
+                                    writeBuffer.put((byte) '\n');
+                                }
+                            }catch (Exception e) {
+                                map.remove(clientChannel);
+                                key.cancel();
+                            }
+                        }
+                        else if (key.isWritable()) {
+                        	// when the key is writeable, it will write the content in buffer to client socket channel
+                            SocketChannel clientChannel = (SocketChannel) key.channel();
+                            ByteBuffer writeBuffer = map.get(clientChannel);
+                            // if the writeBuffer is empty or null
+                            if (writeBuffer == null || writeBuffer.position() == 0) {
+                                continue;
+                            }
+                            writeBuffer.flip();
+                            clientChannel.write(writeBuffer);
+                            writeBuffer.clear();
+                        }
+                    }
+                    finally {
+                    	// go to the next key
+                        keyIterator.remove();
+                    }
+                }
+            }
+            catch (IOException ioe) {
+                System.out.println("Something wrong! Program terminates");
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    @Override
+    public String getRandomNumberOfNextFoes(String difficulty) {
+        // default value is easy
+        if (difficulty == null) {
+            return String.valueOf(RANDOM.nextInt(Difficulty.Easy.getLevel() + 1));
+        }
+        for (Difficulty dif: Difficulty.values()) {
+            if (dif.name().equals(difficulty)) {
+                return String.valueOf(RANDOM.nextInt(dif.getLevel() + 1));
+            }
+        }
+        return String.valueOf(RANDOM.nextInt(Difficulty.Easy.getLevel() + 1));
 
     }
 
+    @Override
+    public String getRandomNextMove() {
+        int move = RANDOM.nextInt(80);
+        if (move < 20) {
+            return "Up";
+        }
+        if (move >= 20 && move < 40) {
+            return "Down";
+        }
+        if (move == 40 || move == 41) {
+            return "Right";
+        }
+        else {
+            return "Left";
+        }
+    }
 }
